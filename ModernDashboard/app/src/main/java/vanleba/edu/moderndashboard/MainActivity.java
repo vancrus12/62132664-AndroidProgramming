ca3a5e06eafb81dd878c5cf116bb8af7631d901c
Lê Bá Văn <vancrus12@gmail.com>
2025-03-14 16:21:47 +0700

    Update MainActivity.java

--- ModernDashboard/app/src/main/java/vanleba/edu/moderndashboard/MainActivity.java
+++ ModernDashboard/app/src/main/java/vanleba/edu/moderndashboard/MainActivity.java
@@ -1,24 +1,29 @@
 package vanleba.edu.moderndashboard;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView clothingCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        clothingCard = findViewById(R.id.clothingCard);
+
+        clothingCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ClothingActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
