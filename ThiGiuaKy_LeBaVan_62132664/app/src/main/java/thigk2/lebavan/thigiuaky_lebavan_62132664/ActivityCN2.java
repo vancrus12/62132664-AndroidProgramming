418ac99e93bf365f962e47c11dcc6a9668c74083
Lê Bá Văn <vancrus12@gmail.com>
2025-04-04 14:33:59 +0700

    updated Cn2

--- ThiGiuaKy_LeBaVan_62132664/app/src/main/java/thigk2/lebavan/thigiuaky_lebavan_62132664/ActivityCN2.java
+++ ThiGiuaKy_LeBaVan_62132664/app/src/main/java/thigk2/lebavan/thigiuaky_lebavan_62132664/ActivityCN2.java
@@ -1,6 +1,9 @@
 package thigk2.lebavan.thigiuaky_lebavan_62132664;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,20 @@ public class ActivityCN2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cn2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        EditText edtNum1 = findViewById(R.id.editTextGK);
+        EditText edtNum2 = findViewById(R.id.editTextCK);
+        Button btnTB = findViewById(R.id.btnTinhTB);
+        TextView tvResult = findViewById(R.id.txtKetQua);
+        btnTB.setOnClickListener(v -> {
+            try {
+                double num1 = Double.parseDouble(edtNum1.getText().toString());
+                double num2 = Double.parseDouble(edtNum2.getText().toString());
+                double tb = (num1 + num2)/2;
+                tvResult.setText("Diem TB: " + tb);
+            } catch (NumberFormatException e) {
+                tvResult.setText("Vui lòng nhập số hợp lệ!");
+            }
         });
     }
 }
