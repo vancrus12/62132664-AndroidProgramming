1c50d30bb00fe12c078c23a72837973a3489a56d
Lê Bá Văn <vancrus12@gmail.com>
2026-03-16 02:09:46 +0700

    create

---
+++ BTH4_LinearLayout_Tong2So/app/src/main/java/ntu/lebavan/bth4_linearlayout_tong2so/MainActivity.java
@@ -0,0 +1,18 @@
+package ntu.lebavan.bth4_linearlayout_tong2so;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
