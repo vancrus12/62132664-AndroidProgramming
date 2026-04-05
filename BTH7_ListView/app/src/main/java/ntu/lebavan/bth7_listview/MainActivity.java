f52e7150eaa46e2bf4fc9a8ca0b1c698dc736d89
Lê Bá Văn <vancrus12@gmail.com>
2026-04-06 04:48:49 +0700

    Hoan thanh

--- BTH7_ListView/app/src/main/java/ntu/lebavan/bth7_listview/MainActivity.java
+++ BTH7_ListView/app/src/main/java/ntu/lebavan/bth7_listview/MainActivity.java
@@ -1,8 +1,11 @@
 package ntu.lebavan.bth7_listview;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,13 +17,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<String> dsTinhThanh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<String> dsTinhThanh;
         dsTinhThanh = new ArrayList<String>();
 
         dsTinhThanh.add("Ha Noi");
@@ -36,6 +38,13 @@ public class MainActivity extends AppCompatActivity {
 
         ListView lvTenTinhThanh = findViewById(R.id.lvDSTT);
         lvTenTinhThanh.setAdapter(adapterTinhThanh);
+        lvTenTinhThanh.setOnItemClickListener(BoLangNghevaXL);
     }
-
+    AdapterView.OnItemClickListener BoLangNghevaXL = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            String strTenTinhChon = dsTinhThanh.get(i);
+            Toast.makeText(MainActivity.this, strTenTinhChon, Toast.LENGTH_LONG).show();
+        }
+    };
 }
