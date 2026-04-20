89461056235bec7c8c23daa3fadf536aeabe0ea7
Lê Bá Văn <vancrus12@gmail.com>
2026-04-20 08:00:44 +0700

    COMMIT LẦN THỨ 2

---
+++ BaiThiGK2/app/src/main/java/thigk2/lebavan/baithigk2/ChucNang1.java
@@ -0,0 +1,64 @@
+package thigk2.lebavan.baithigk2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ChucNang1#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ChucNang1 extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ChucNang1() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ChucNang1.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ChucNang1 newInstance(String param1, String param2) {
+        ChucNang1 fragment = new ChucNang1();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chuc_nang1, container, false);
+    }
+}
--- BaiThiGK2/app/src/main/java/thigk2/lebavan/baithigk2/MainActivity.java
+++ BaiThiGK2/app/src/main/java/thigk2/lebavan/baithigk2/MainActivity.java
@@ -1,24 +1,42 @@
 package thigk2.lebavan.baithigk2;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        BottomNavigationView bottomNav  = findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                int itemId = item.getItemId();
+                if (itemId == R.id.navigation_chucnang1) {
+                    selectedFragment = new ChucNang1();
+                } else if (itemId == R.id.navigation_chucnang2) {
+                    selectedFragment = new ChucNang2();
+                } else if (itemId == R.id.navigation_chucnang3) {
+                    selectedFragment = new ChucNang3();
+                } else if (itemId == R.id.navigation_chucnang4) {
+                    selectedFragment = new ChucNang4();
+                    // It will help to replace the
+                    // one fragment to other.
+                    if (selectedFragment != null) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentSpace, selectedFragment).commit();
+                    }
+                    return true;
+                }
+            });
+        }
     }
-}
